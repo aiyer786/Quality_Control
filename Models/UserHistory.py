746aa7306ce3b1e2c392b2b917453dee0c63d3b4
adatta4 <adatta4@ncsu.edu>
2023-12-01 17:44:49 -0500

    user_data.csv created which stores all tag details of all users

--- Models/UserHistory.py
+++ Models/UserHistory.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-from datetime import datetime
-
-class AnswerTag:
+class UserHistory:
     # This class represents an AnswerTag object, which stores information about a specific tag associated with an answer.
-    def __init__(self, id: int, assignment_id: int, answer_id: int, tag_prompt_deployment_id: int, user_id: int, 
-                value: str, created_at: datetime, updated_at: datetime, tag_prompt_id = None) -> None:
+    def __init__(self, id: int, question_id: int, question: str, assignment_id: int, answer_id: int, tag_prompt_deployment_id: int, user_id: int, 
+                value: str, created_at: datetime, updated_at: datetime, tag_prompt_id: None, comments: str, prompt: str) -> None:
         self.id = id
+        self.question_id = question_id
+        self.question = question
         self.assignment_id = assignment_id # - assignment_id: an integer representing the ID of the assignment associated with this AnswerTag
         self.answer_id = answer_id # - answer_id: an integer representing the ID of the answer associated with this AnswerTag
         self.tag_prompt_deployment_id = tag_prompt_deployment_id # - tag_prompt_deployment_id: an integer representing the ID of the tag prompt deployment associated with this AnswerTag
@@ -15,3 +15,5 @@ class AnswerTag:
         self.created_at = created_at #.strftime('%Y-%m-%d %H:%M:%S') 
         self.updated_at = updated_at
         self.tag_prompt_id = tag_prompt_id # - tag_prompt_id: an integer representing the ID of the tag prompt associated with this AnswerTag
+        self.comments = comments
+        self.prompt = prompt
